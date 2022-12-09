4c8aa2477bf3ac515577028d62ad5563ff97fdf4
Melisa Z <melzahirovic@gmail.com>
2022-12-08 22:43:36 -0600

    added package.json

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const session = require('express-session');
 const routes = require('./controllers');
-const exphbs = require('express-handlebars');
 const path = require('path');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
